41c3ad638198300ecf129a313516df5decbe22a8
IrenaRndl <irena.rndl@gmail.com>
2019-03-26 22:20:46 +0200

    My second commit

--- task 2.1.py
+++ task 2.1.py
@@ -2,5 +2,5 @@ text=input('Enter some text please\n')
 word=input('Enter one more word please\n')
 n=text.count(word)
 final_text=f"Слово {word} встречается в тексте {n} раз"
-
+#comment
 print(final_text)
